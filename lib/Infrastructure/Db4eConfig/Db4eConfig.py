b63523f780cfd4dc53307618873eb59b77573709
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2025-05-30 15:10:44 -0400

    Cleaned up the usage message for db4e.py

--- lib/Infrastructure/Db4eConfig/Db4eConfig.py
+++ lib/Infrastructure/Db4eConfig/Db4eConfig.py
@@ -16,10 +16,22 @@ class Db4eConfig():
 
         # Setup the command line parser
         parser = argparse.ArgumentParser(description='DB4E Configuration')
-        help_msg = 'Valid actions are [backup_db|monitor_p2pool_log|new_blocks_found_csv|new_p2pool_payment_csv|new_shares_found_csv|new_shares_found_by_host|get_wallet_balance]'
-        parser.add_argument('-a', '--action', default='None', type=str, help=help_msg)
+        
+        parser.add_argument('-a', '--action', default='None', type=str, help='Do -la to list all actions. ')
+        parser.add_argument('-la', '--list_actions', action='store_true', help='List all available actions.')
 
         args = parser.parse_args()
+        if args.list_actions:
+            print("Available actions:")
+            print("  new_blocks_found_csv     : Generate the 'Blocks Found' CSV files and push them to GitHub.")
+            print("  new_p2pool_payment_csv   : Generate the 'P2Pool Payments' CSV files and push them to GitHub.")
+            print("  new_shares_found_csv     : Generate the 'Shares Found' CSV files and push it them GitHub.")
+            print("  new_shares_found_by_host : Generate the 'Shares Found by Host' CSV files and push them to GitHub.")
+            print("  backup_db                : Backup MongoDB and push the backup to GitHub.")
+            print("  monitor_p2pool_log       : Start monitoring the P2Pool daemon log.")
+            print("  get_wallet_balance       : Get the wallet balance from MongoDb.")
+            exit(0)
+
         # Parse any command line args
         self.config['db4e']['action'] = args.action
 
