708b7f6f40b110448f1256dfebcd994c8cb0657d
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2025-07-08 22:13:10 -0400

    Modified for await / async

--- db4e/Modules/PaneMgr.py
+++ db4e/Modules/PaneMgr.py
@@ -6,7 +6,7 @@ db4e/Modules/PaneMgr.py
    Copyright (c) 2024-2025 NadimGhaznavi <https://github.com/NadimGhaznavi/db4e>
    License: GPL 3.0
 """
-
+import inspect
 from dataclasses import dataclass, field
 from textual.css.query import NoMatches
 from textual.widget import Widget
@@ -15,6 +15,7 @@ from textual.reactive import reactive
 
 from db4e.Modules.ConfigMgr import Config
 from db4e.Modules.PaneCatalogue import PaneCatalogue
+from db4e.Modules.Helper import get_effective_identity
 from db4e.Messages.UpdateTopBar import UpdateTopBar
 from db4e.Constants.Panes import INITIAL_SETUP_PANE, WELCOME_PANE, RESULTS_PANE
 
@@ -34,7 +35,6 @@ class PaneMgr(Widget):
         self.panes = {}
 
     def compose(self):
-        print(f"PaneMgr:compose(): {self.pane_state}")
         with ContentSwitcher(initial=self.pane_state.name, id="content_switcher"):
             for pane_name in self.catalogue.registry:
                 # Instantiate each pane once, store a reference
@@ -43,30 +43,30 @@ class PaneMgr(Widget):
                 yield pane
 
     async def on_mount(self) -> None:
-        print(f"PaneMgr:on_mount(): {self.pane_state}")
         initial = PaneState(name=WELCOME_PANE if self.initialized_flag else INITIAL_SETUP_PANE, data={})
-        self.set_pane(initial.name, initial.data)
+        if initial.name == INITIAL_SETUP_PANE:
+            initial.data = get_effective_identity()
+        await self.set_pane(initial.name, initial.data)
 
-    def set_pane(self, name: str, data: dict | None = None):
+    async def set_pane(self, name: str, data: dict | None = None):
         if not self.initialized_flag and name != RESULTS_PANE:
             self.pane_state = PaneState(INITIAL_SETUP_PANE, data)
+        elif name == RESULTS_PANE and not data:
+            self.pane_state = PaneState(WELCOME_PANE, {})
         else:
             self.pane_state = PaneState(name, data)
         # If the pane supports set_data, update it with new data
-        print(f"PaneMgr:set_pane(): {self.pane_state}")
         if data and name in self.panes:
             pane = self.panes[name]
             if hasattr(pane, "set_data"):
-                pane.set_data(data)
+                await pane.set_data(data)
 
     def watch_pane_state(self, old: PaneState, new: PaneState):
         try:
             content_switcher = self.query_one("#content_switcher", ContentSwitcher)
         except NoMatches:
             return
-        
         content_switcher.current = new.name
-
         # Create a message to update the TopBar's title and sub_title
         title, sub_title = self.catalogue.get_metadata(new.name)
         self.post_message(UpdateTopBar(self, title=title, sub_title=sub_title))
