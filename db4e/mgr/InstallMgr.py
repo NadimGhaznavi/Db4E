3bf8ea2b5db4111099112f274505d7820fda3618
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-04-23 11:54:06 -0400

    Fixed: InstallMgr ping test

--- db4e/mgr/InstallMgr.py
+++ db4e/mgr/InstallMgr.py
@@ -204,16 +204,16 @@ class InstallMgr(Container):
 
             # Confirm that the Db4E service is up
             max_tries = 20
-            count = 0
+            count = 1
             ping_successful = False
-            while count != max_tries:
-                count += 1
+            while count < max_tries:
                 if self.sync_client.ping():
-                    max_tries = 0
+                    count = max_tries
                     ping_successful = True
                 else:
                     time.sleep(1)
                     print(f"Waiting for the Db4E service, count: {count}")
+                count += 1
 
             if not ping_successful:
                 self.ops_db.add_tui_log_line(
