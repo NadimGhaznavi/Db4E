314542e11dbcf3f603317a9336bb641e6bcae32f
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-04-22 23:43:51 -0400

    Feat: TIghter sync

--- scripts/qasync.sh
+++ scripts/qasync.sh
@@ -1,8 +1,11 @@
-#!/bin/bash 
-#
+#!/bin/bash
+set -euo pipefail
 
 SRC=/opt/qa/db4e/db4e/
 DEST=/home/dan/db4e_venv/lib/python3.11/site-packages/db4e/
 
-mkdir -p $DEST
-rsync -avr --delete "$SRC" "$DEST" | grep -v __pycache__
+mkdir -p "$DEST"
+rsync -avr --delete \
+  --exclude='__pycache__/' \
+  --exclude='*.pyc' \
+  "$SRC" "$DEST"Yeah
